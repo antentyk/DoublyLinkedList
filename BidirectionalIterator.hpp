41fb8ecd4993c86f14d242ff451e3b34db644cf6
george <antentyk@ucu.edu.ua>
2018-05-15 13:36:16 +0300

    BidirectionalIterator moved to a separate file

---
+++ BidirectionalIterator.hpp
@@ -0,0 +1,57 @@
+#ifndef BIDIRECTIONAL_ITERATOR_
+#define BIDIRECTIONAL_ITERATOR_
+
+template<class T>
+class DoublyLinkedList<T>::BidirectionalIterator{
+public:
+    friend class DoublyLinkedList<T>;
+
+    typedef ptrdiff_t difference_type;
+    typedef T value_type;
+    typedef T* pointer;
+    typedef T& reference;
+    typedef std::bidirectional_iterator_tag iterator_category;
+
+    BidirectionalIterator(Node<T> *data):
+        data(data)
+    {}
+
+    inline bool operator==(const BidirectionalIterator &other){
+        return &(*data) == &(*other.data);
+    }
+    inline bool operator!=(const BidirectionalIterator &other){
+        return !operator==(other);
+    }
+
+    inline T& operator*() {
+        return *data->getValue();
+    }
+
+    BidirectionalIterator& operator++(){
+        if(data->getRight() != nullptr)
+            data = data->getRight();
+        return *this;
+    }
+    BidirectionalIterator operator++(int){
+        BidirectionalIterator tmp(*this);
+        if(data->getRight() != nullptr)
+            data = data->getRight();
+        return tmp;
+    }
+
+    BidirectionalIterator& operator--(){
+        if(data->getLeft() != nullptr)
+            data = data->getLeft();
+        return *this;
+    }
+    BidirectionalIterator operator--(int){
+        BidirectionalIterator tmp(*this);
+        if(data->getLeft() != nullptr)
+            data = data->getLeft();
+        return tmp;
+    }
+private:
+    Node<T> *data;
+};
+
+#endif
--- DoublyLinkedList.hpp
+++ DoublyLinkedList.hpp
@@ -10,57 +10,7 @@
 template<class T>
 class DoublyLinkedList{
 public:
-    class BidirectionalIterator{
-    public:
-        friend class DoublyLinkedList<T>;
-
-        typedef ptrdiff_t difference_type;
-        typedef T value_type;
-        typedef T* pointer;
-        typedef T& reference;
-        typedef std::bidirectional_iterator_tag iterator_category;
-
-        BidirectionalIterator(Node<T> *data):
-            data(data)
-        {}
-
-        inline bool operator==(const BidirectionalIterator &other){
-            return &(*data) == &(*other.data);
-        }
-        inline bool operator!=(const BidirectionalIterator &other){
-            return !operator==(other);
-        }
-
-        inline T& operator*() {
-            return *data->getValue();
-        }
-
-        BidirectionalIterator& operator++(){
-            if(data->getRight() != nullptr)
-                data = data->getRight();
-            return *this;
-        }
-        BidirectionalIterator operator++(int){
-            BidirectionalIterator tmp(*this);
-            if(data->getRight() != nullptr)
-                data = data->getRight();
-            return tmp;
-        }
-
-        BidirectionalIterator& operator--(){
-            if(data->getLeft() != nullptr)
-                data = data->getLeft();
-            return *this;
-        }
-        BidirectionalIterator operator--(int){
-            BidirectionalIterator tmp(*this);
-            if(data->getLeft() != nullptr)
-                data = data->getLeft();
-            return tmp;
-        }
-    private:
-        Node<T> *data;
-    };
+    class BidirectionalIterator;
 
     typedef BidirectionalIterator iterator;
 
@@ -105,5 +55,7 @@ public:
 private:
     Node<T> *head, *tail;
 };
+
+#include "BidirectionalIterator.hpp"
 
 #endif
